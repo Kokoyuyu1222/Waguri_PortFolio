be78d4c3382eb167321f9475d7f93d16f899f4c4
kokoyuyu1222 <yk.rk1222@gmail.com>
2020-04-05 10:58:39 +0000

    devise view削除

--- config/routes.rb
+++ config/routes.rb
@@ -27,38 +27,38 @@ Rails.application.routes.draw do
     get 'orders/confirm' => 'orders#confirm'
     get 'orders/finish' => 'orders#finish'
     resources :orders ,only: [:new,:create,:index,:show]
-  delete 'consumers/cart_products/destroy_all'  => 'cart_products#destroy_all'
-end
+    delete 'consumers/cart_products/destroy_all'  => 'cart_products#destroy_all'
+  end
   namespace :fermers do
-  get 'homes/top'
-  get '/search' => 'searches#search'
-  resources :searches,only: [:index]
-  resources :products
-  resources :columes
-  resources :columes_comments,only: [:index,:show]
-  resources :columes_favorites,only: [:index]
-  resources :products_comments,only: [:index,:show]
-  resources :products_favorites,only: [:index]
-  resources :order_products,only: [:update]
- resources :orders,only: [:show,:index,:update]
- resources :consumers,only: [:show,:index]
- resources :addresses ,only: [:show,:index,:edit,:update,:create,:destroy]
-end
+    get 'homes/top'
+    get '/search' => 'searches#search'
+    resources :searches,only: [:index]
+    resources :products
+    resourc es :columes
+    resources :columes_comments,only: [:index,:show]
+    resources :columes_favorites,only: [:index]
+    resources :products_comments,only: [:index,:show]
+    resources :products_favorites,only: [:index]
+    resources :order_products,only: [:update]
+    resources :orders,only: [:show,:index,:update]
+    resources :consumers,only: [:show,:index]
+    resources :addresses ,only: [:show,:index,:edit,:update,:create,:destroy]
+  end
 
-namespace :admins do
-  get 'homes/top'
-  get '/search' => 'searches#search'
-  resources :searches,only: [:index]
-  resources :categories,only: [:index,:create,:edit,:update,:destroy]
-  resources :brands,only: [:index,:create,:edit,:update,:destroy]
-  resources :products,only: [:index,:show,:destroy]
- resources :orders,only: [:show,:index,:destroy]
- resources :members,only: [:show,:index,:edit,:update]
- resources :columes,omly: [:index,:show,:destroy]
- resources :columes_comments,only: [:index,:show]
-  resources :products_comments,only: [:index,:show]
-end
-root 'homes#top'
+  namespace :admins do
+    get 'homes/top'
+    get '/search' => 'searches#search'
+    resources :searches,only: [:index]
+    resources :categories,only: [:index,:create,:edit,:update,:destroy]
+    resources :brands,only: [:index,:create,:edit,:update,:destroy]
+    resources :products,only: [:index,:show,:destroy]
+    resources :orders,only: [:show,:index,:destroy]
+    resources :members,only: [:show,:index,:edit,:update]
+    resources :columes,omly: [:index,:show,:destroy]
+    resources :columes_comments,only: [:index,:show]
+    resources :products_comments,only: [:index,:show]
+  end
+  root 'homes#top'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

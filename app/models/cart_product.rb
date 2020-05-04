5820ccc951189a35ea64c0fd06d6662e9c798750
kokoyuyu1222 <yk.rk1222@gmail.com>
2020-05-04 13:39:45 +0000

    validate stock quantity error/message

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -5,9 +5,9 @@ class CartProduct < ApplicationRecord
   	validate :validate_stock_quantity_check, on: [:new, :create]
 
     def validate_stock_quantity_check
-    	stock_quantity = product.stock.quantity # 在庫数
+    	stock_quantity = product.stock.quantity 
     	if self.quantity > stock_quantity
-    		errors.add(:quantity, "申し訳ありません。購入できるのは#{stock_quantity}個までです")
+    		record.errors.add(:quantity, "申し訳ありません。購入できるのは#{stock_quantity}個までです")
     	end
     end
 end
